d3fd6e1e22918059fa6db8e2f8b205c18db7d316
George Higbie <gehigbie@gmail.com>
2023-09-17 10:23:47 -0500

    Add patch and delete endpoints

--- src/index.js
+++ src/index.js
@@ -35,11 +35,31 @@ app.post("/notes", async (req, res) => {
   }
 });
 
-app.put("/notes", async (req, res) => {
+app.patch("/notes/:id", async (req, res) => {
   try {
-    const { id, note } = req.body;
-    const updatedNote = await Note.findByIdAndUpdate(id, { note }).save();
-    res.status(200).send(updatedNote).redirect("/notes");
+    const { id } = req.params;
+    const { note } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      id,
+      { note },
+      { new: true }
+    );
+    if (!updatedNote) {
+      return res.status(404).send({ error: "Note not found" });
+    }
+    res.status(200).send(updatedNote);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.delete("/notes/:id", async (req, res) => {
+  try {
+    const note = await Note.findByIdAndDelete(req.params.id);
+    if (!note) {
+      return res.status(404).send({ error: "Note not found" });
+    }
+    res.status(200).send({ message: "Note deleted" });
   } catch (err) {
     res.status(400).send(err);
   }
