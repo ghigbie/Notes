1f03e8c4a0650a1acee7f81ba7a695892b7d842b
George Higbie <gehigbie@gmail.com>
2023-09-17 09:10:28 -0500

    Intial commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const fs = require("fs");
+require("./db/mongoose");
+const app = express();
+const Note = require("./models/note");
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get("/notes-hard", (req, res) => {
+  fs.readFile(`${__dirname}/notes.json`, "utf-8", (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.status(200).send(data);
+  });
+});
+
+app.get("/notes", async (req, res) => {
+  try {
+    const notes = await Note.find({});
+    res.status(200).send(notes);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.post("/notes", async (req, res) => {
+  const note = new Note(req.body);
+  try {
+    await note.save();
+    res.status(200).send(note);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.put("/notes", async (req, res) => {
+  try {
+    const { id, note } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(id, { note }).save();
+    res.status(200).send(updatedNote).redirect("/notes");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
